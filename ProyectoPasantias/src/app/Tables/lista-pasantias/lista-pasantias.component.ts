1bf5a49262d469859c9f0855cbaa09f32bf8d4c0
Carolina Ricagno <carom2896@gmail.com>
2020-08-28 21:30:21 -0300

    Se agrega tabla para ver datos de pasantia

--- ProyectoPasantias/src/app/Tables/lista-pasantias/lista-pasantias.component.ts
+++ ProyectoPasantias/src/app/Tables/lista-pasantias/lista-pasantias.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Pasantia } from '../../Models/Pasantia';
 
+
 @Component({
   selector: 'app-lista-pasantias',
   templateUrl: './lista-pasantias.component.html',
@@ -8,12 +9,27 @@ import { Pasantia } from '../../Models/Pasantia';
 })
 export class ListaPasantiasComponent implements OnInit {
   @Input() InputPasantias: Pasantia[] = [];
+
+  verPasantias: boolean=true;
+  verInfoPasantia: boolean = false;
+  pasantiaSeleccionada: Pasantia;
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
   verDatosPasantia(pas: any){
-    console.log('Pasantia --> ' + pas);
+    this.pasantiaSeleccionada = pas;
+    this.verPasantias = false;
+    this.verInfoPasantia = true;
+    console.log('Pasantia --> ');
+    console.log(pas)
   }
+
+  volver(){
+    this.verInfoPasantia = false;
+    this.verPasantias = true;
+  }
+
 }
